d859fb9c2528393b4470839f79d4031d222d50c5
Matthew Sells <matthew.sells@proton.me>
2025-04-04 08:48:18 -0500

    Refactor HTML tag macros to use AST walking

--- lib/mitta/html.ex
+++ lib/mitta/html.ex
@@ -4,54 +4,29 @@ defmodule Mitta.HTML do
            line |> String.trim() |> String.to_atom()
          end)
 
-  for tag <- @tags do
-    defmacro unquote(tag)(attrs, do: inner) do
-      tag = unquote(tag)
-      quote do: tag(unquote(tag), unquote(attrs), do: unquote(inner))
-    end
-
-    defmacro unquote(tag)(attrs) do
-      tag = unquote(tag)
-      quote do: tag(unquote(tag), unquote(attrs))
-    end
-  end
-
   defmacro markup(do: block) do
     quote do
-      import Kernel, except: [div: 2]
-      {:ok, var!(buffer, Mitta.HTML)} = start_buffer([])
-      unquote(block)
-      result = render(var!(buffer, Mitta.HTML))
-      :ok = stop_buffer(var!(buffer, Mitta.HTML))
+      {:ok, var!(buffer, unquote(__MODULE__))} = start_buffer([])
+      unquote(Macro.postwalk(block, &postwalk/1))
+      result = render(var!(buffer, unquote(__MODULE__)))
+      :ok = stop_buffer(var!(buffer, unquote(__MODULE__)))
       result
     end
   end
 
-  defmacro tag(name, attrs \\ []) do
-    {inner, attrs} = Keyword.pop(attrs, :do)
-    quote do: tag(unquote(name), unquote(attrs), do: unquote(inner))
+  def postwalk({:text, _meta, [string]}) do
+    quote do: put_buffer(var!(buffer, unquote(__MODULE__)), to_string(unquote(string)))
   end
 
-  defmacro tag(name, attrs, do: inner) do
-    quote do
-      put_buffer(var!(buffer, Mitta.HTML), open_tag(unquote_splicing([name, attrs])))
-      unquote(inner)
-      put_buffer(var!(buffer, Mitta.HTML), "</#{unquote(name)}>")
-    end
+  def postwalk({tag_name, _meta, [[do: inner]]}) when tag_name in @tags do
+    quote do: tag(unquote(tag_name), [], do: unquote(inner))
   end
 
-  defmacro text(string) do
-    quote do
-      put_buffer(var!(buffer, Mitta.HTML), to_string(unquote(string)))
-    end
+  def postwalk({tag_name, _meta, [attrs, [do: inner]]}) when tag_name in @tags do
+    quote do: tag(unquote(tag_name), unquote(attrs), do: unquote(inner))
   end
 
-  def open_tag(name, []), do: "<#{name}>"
-
-  def open_tag(name, attrs) do
-    attr_html = for {key, val} <- attrs, into: "", do: " #{key}=\"#{val}\""
-    "<#{name}#{attr_html}>"
-  end
+  def postwalk(ast), do: ast
 
   def start_buffer(state) do
     Agent.start_link(fn -> state end)
@@ -69,5 +44,25 @@ defmodule Mitta.HTML do
     Agent.get(buffer, & &1)
     |> Enum.reverse()
     |> Enum.join()
+  end
+
+  defmacro tag(name, attrs \\ []) do
+    {inner, attrs} = Keyword.pop(attrs, :do)
+    quote do: tag(unquote(name), unquote(attrs), do: unquote(inner))
+  end
+
+  defmacro tag(name, attrs, do: inner) do
+    quote do
+      put_buffer(var!(buffer, unquote(__MODULE__)), open_tag(unquote_splicing([name, attrs])))
+      unquote(inner)
+      put_buffer(var!(buffer, unquote(__MODULE__)), "</#{unquote(name)}>")
+    end
+  end
+
+  def open_tag(name, []), do: "<#{name}>"
+
+  def open_tag(name, attrs) do
+    attr_html = for {key, val} <- attrs, into: "", do: " #{key}=\"#{val}\""
+    "<#{name}#{attr_html}>"
   end
 end
